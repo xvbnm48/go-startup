f793441340befaad24ca6e9713dca53a22c8e5c8
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-11-10 01:19:22 +0700

    menghubungkan ke database

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go-startup/user"
 	"log"
 
 	"gorm.io/driver/mysql"
@@ -10,10 +11,25 @@ import (
 
 func main() {
 	dsn := "root:@tcp(127.0.0.1:3306)/go-startup?charset=utf8mb4&parseTime=True&loc=Local"
-	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	fmt.Println("connection to database is good")
+
+	var users []user.User
+	length := len(users)
+	fmt.Println(length)
+
+	db.Find(&users)
+	length = len(users)
+	fmt.Println(length)
+
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+		fmt.Println("==============")
+	}
+
 }
