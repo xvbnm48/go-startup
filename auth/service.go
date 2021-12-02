05fe07898c3bb17750d28701ee0e9b5dcad9c482
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2021-12-02 21:09:38 +0700

    berhasil membuat validasi token

--- auth/service.go
+++ auth/service.go
@@ -1,11 +1,16 @@
 package auth
 
-import "github.com/golang-jwt/jwt"
+import (
+	"errors"
+
+	"github.com/golang-jwt/jwt"
+)
 
 // service ini mengcreate JWT dan Memvalidasi token JWT
 
 type Service interface {
 	GenerateToken(userID int) (string, error)
+	ValidateToken(encodedToken string) (*jwt.Token, error)
 }
 
 type jwtService struct {
@@ -31,5 +36,23 @@ func (s *jwtService) GenerateToken(userID int) (string, error) {
 		return signedToken, err
 	}
 	return signedToken, nil
+
+}
+
+func (s *jwtService) ValidateToken(encodedToken string) (*jwt.Token, error) {
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("invalid token")
+		}
+
+		return []byte(SECRET_KEY), nil
+	})
+	if err != nil {
+		return token, err
+
+	}
+
+	return token, nil
 
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"go-startup/auth"
 	"go-startup/handler"
 	"go-startup/user"
@@ -22,6 +23,17 @@ func main() {
 	userService.SaveAvatar(1, "images/avatar.jpg")
 	authService := auth.NewService()
 	//fmt.Println(authService.GenerateToken(10))
+
+	token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxN30.9AhPIz7AyPWBJyk0sb6uFuN2QrPctGRr45n1l7jxaL4")
+	if err != nil {
+		fmt.Println("error")
+	}
+
+	if token.Valid {
+		fmt.Println("valid")
+	} else {
+		fmt.Println("invalid")
+	}
 
 	userHandler := handler.NewUserHandler(userService, authService)
 
